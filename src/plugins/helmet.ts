69d4231134deb2c5e6be5ca44e2aa664d9684591
Pierre Berger <pierre@fleex.com>
2024-01-26 19:41:08 +0100

    feat: add helmet plugin

---
+++ src/plugins/helmet.ts
@@ -0,0 +1,15 @@
+import fp from 'fastify-plugin'
+
+import type { FastifyHelmetOptions } from '@fastify/helmet'
+import helmet from '@fastify/helmet'
+
+/**
+ * @fastify/helmet enables the use of helmet in a Fastify application.
+ *
+ * @see https://github.com/fastify/fastify-helmet
+ */
+export default fp<FastifyHelmetOptions>(async (fastify, opts) => {
+  await fastify.register(helmet, {
+    ...opts,
+  })
+})
