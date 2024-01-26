6e9e422e5832d6b11e7297aa345e344247ad34e0
Pierre Berger <pierre@fleex.com>
2024-01-26 19:43:43 +0100

    feat: add sensible plugin

---
+++ src/plugins/sensible.ts
@@ -0,0 +1,12 @@
+import fp from 'fastify-plugin'
+import type { SensibleOptions } from '@fastify/sensible'
+import sensible from '@fastify/sensible'
+
+/**
+ * This plugins adds some utilities to handle http errors
+ *
+ * @see https://github.com/fastify/fastify-sensible
+ */
+export default fp<SensibleOptions>(async fastify => {
+  fastify.register(sensible)
+})
